d853b1aee19efd1b70cfbc7dd7dc8b9a9c60a6e7
999max <999max@gmail.com>
2021-01-11 19:10:53 +0300

    updated admin.py (added fields for choices and other)

--- polls_app/admin.py
+++ polls_app/admin.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 from .models import Poll, Choice
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+    exclude = ('votes', 'passed_users')
+
+
+class PollAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    exclude = ('date_added', 'passed_users',)
+
+
 class ChoiceAdmin(admin.ModelAdmin):
     readonly_fields = ('votes', )
 
 
-admin.site.register(Poll)
+admin.site.register(Poll, PollAdmin)
 admin.site.register(Choice, ChoiceAdmin)
