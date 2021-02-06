7bbbb7c786e0d14b7da1c0fe5ae9c39ac0404f63
999max <999max@gmail.com>
2021-02-06 23:03:22 +0300

    Add test for ResultView, add test for PollView (both with choices)

--- polls_app/tests/tests_views.py
+++ polls_app/tests/tests_views.py
@@ -1,8 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.utils import timezone
 
 from polls_app.models import Poll, Choice
+from polls_app.views import make_vote, make_text_vote
 from django.urls import reverse
+
 
 
 class IndexTestView(TestCase):
@@ -69,16 +71,31 @@ class PollsTestView(TestCase):
 
 class PollTestView(TestCase):
     def setUp(self):
-        self.poll_1 = Poll.objects.create(title=f"poll_zero", question_text=f"question poll_zero",
+        self.poll_1 = Poll.objects.create(title="poll_zero", question_text="question poll_zero",
                                           date_ended=timezone.now(),
                                           answer_type='1')
-        self.poll_2 = Poll.objects.create(title=f"poll_2", question_text=f"question poll_2",
+        self.poll_2 = Poll.objects.create(title="poll_2", question_text="question poll_2",
                                           date_ended=timezone.now() + timezone.timedelta(seconds=10),
                                           answer_type='1')
+        self.choice_1_1 = Choice.objects.create(question=self.poll_1, choice_text="choice 11")
+        self.choice_1_2 = Choice.objects.create(question=self.poll_1, choice_text="choice 12")
+        self.choice_2_1 = Choice.objects.create(question=self.poll_2, choice_text="choice 21")
+        self.choice_2_2 = Choice.objects.create(question=self.poll_2, choice_text="choice 22")
+
+    def test_poll_view_exists(self):
+        for i in range(1, 3):
+            response = self.client.get(f"/polls/poll/{i}/")
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, 'polls_app/poll.html')
 
     def test_poll_view_get_by_name(self):
         response = self.client.get(reverse('polls_app:poll', kwargs={'pk': 1}))
         self.assertEqual(response.status_code, 200)
+
+    def test_poll_view_does_not_exist(self):
+        for i in range(3, 10):
+            response = self.client.get(f"/polls/poll/{i}/")
+            self.assertNotEqual(response.status_code, 200)
 
     def test_poll_is_closed_for_inactive_poll(self):
         response = self.client.get(reverse('polls_app:poll', kwargs={'pk': 1}))
@@ -89,6 +106,16 @@ class PollTestView(TestCase):
         response = self.client.get(reverse('polls_app:poll', kwargs={'pk': 2}))
         self.assertNotContains(response, 'Voting was closed at')
         self.assertContains(response, 'Make your answer')
+
+    def test_choices_are_exist(self):
+        response = self.client.get(reverse('polls_app:poll', kwargs={'pk': 2}))
+        self.assertContains(response, 'choice 21')
+        self.assertContains(response, 'choice 22')
+
+    def test_choices_for_expired_question_are_not_exist(self):
+        response = self.client.get(reverse('polls_app:poll', kwargs={'pk': 1}))
+        self.assertNotContains(response, 'choice 11')
+        self.assertNotContains(response, 'choice 12')
 
 
 class CompletedPollsTestView(TestCase):
@@ -126,3 +153,39 @@ class CompletedPollsTestView(TestCase):
         response = self.client.get('/polls/completed/')
         self.assertNotContains(response, 'poll_3')
         self.assertNotContains(response, 'poll_4')
+
+
+class ResultsTestView(TestCase):
+    def setUp(self):
+        self.poll_1 = Poll.objects.create(title="poll_zero_1", question_text="question poll_1",
+                                          date_ended=timezone.now(),
+                                          answer_type='N')
+        self.poll_2 = Poll.objects.create(title="poll_zero_2", question_text="question poll_2",
+                                          date_ended=timezone.now(),
+                                          answer_type='1')
+        self.choice_1_1 = Choice.objects.create(question=self.poll_1, choice_text="choice 11")
+        self.choice_1_2 = Choice.objects.create(question=self.poll_1, choice_text="choice 12")
+        self.choice_2_1 = Choice.objects.create(question=self.poll_2, choice_text="choice 21")
+        self.choice_2_2 = Choice.objects.create(question=self.poll_2, choice_text="choice 22")
+
+    def test_results_view_exist(self):
+        for i in range(1, 3):
+            response = self.client.get(f"/polls/{i}/results/")
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.template_name[0], 'polls_app/results.html')
+
+    def test_results_view_get_by_name(self):
+        response = self.client.get(reverse('polls_app:results', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_results_view_does_not_exsit(self):
+        for i in range(3, 7):
+            response = self.client.get(f"/polls/{i}/results/")
+            self.assertNotEqual(response.status_code, 200)
+
+    def test_if_every_cases_are_exist(self):
+        for i in (1, 2):
+            response = self.client.get(reverse('polls_app:results', kwargs={'pk': i}))
+            self.assertContains(response, f"question poll_{i}")
+
+
